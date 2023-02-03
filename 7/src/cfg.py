5e8086e9d416d8fa8f53ef41bc041fe5deade900
sandhi_artha <sandiartha99@yahoo.co.id>
2023-02-03 09:42:49 +0100

    7. model cfg

--- 7/src/cfg.py
+++ 7/src/cfg.py
@@ -3,12 +3,13 @@ class cfg:
     save_dir = 'result/proto/'  # will have: results, saved_models, figures
     
     # model
+    model = 'densenet'
     num_classes = 10
     train_only_task_head = True
     use_pretrained = True
 
     # training
-    bs = 16
+    bs = 32
     lr = 1e-3
     epochs = 3
     save_model = False
