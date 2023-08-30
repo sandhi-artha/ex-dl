8245c7e771aaca4dea220efd0662b9c878d72f2d
sandhi_artha <sandiartha99@yahoo.co.id>
2023-08-30 23:53:04 +0200

    9 cache cifar

--- 9/src/dataset.py
+++ 9/src/dataset.py
@@ -18,3 +18,20 @@ class CifarDS(torch.utils.data.Dataset):
 
     def __len__(self):
         return len(self.df)
+
+class CacheCifarDS(torch.utils.data.Dataset):
+    def __init__(self, cfg: dict, df: pd.DataFrame):
+        self.cfg = cfg
+        self.images = torch.zeros((df.shape[0],3,32,32), dtype=torch.float32)
+        self.labels = torch.zeros((df.shape[0]), dtype=torch.int64)
+
+        for i in range(df.shape[0]):
+            row = df.iloc[i]
+            self.images[i,:,:,:] = torchvision.io.read_image(row['path'])/255.0
+            self.labels[i] = row['label']
+
+    def __getitem__(self, idx):
+        return self.images[idx], self.labels[idx]
+
+    def __len__(self):
+        return len(self.images)
