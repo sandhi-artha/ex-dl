143d652af1a22236704b3659be4ff7f3afec3636
sandhi_artha <sandiartha99@yahoo.co.id>
2023-10-19 18:00:38 +0200

    10. pca stats

--- 10/learn/pca.py
+++ 10/learn/pca.py
@@ -11,6 +11,16 @@ def labels_to_colors(arr: np.ndarray, num_classes: int) -> np.ndarray:
         c_arr[np.argwhere(arr == c)] = cmap.colors[c]
     return c_arr
 
+def pca_stats(arr_red: np.ndarray) -> str:
+    """ xmax | xmin | xmean | xstd
+        ymax | ymin | ymean | ystd
+    """
+    return '{:.3f} | {:.3f} | {:.3f} | {:.3f}\n{:.3f} | {:.3f} | {:.3f} | {:.3f}'.format(
+        np.max(arr_red[:, 0]), np.min(arr_red[:, 0]),
+        np.mean(arr_red[:, 0]), np.std(arr_red[:, 0]),
+        np.max(arr_red[:, 1]), np.min(arr_red[:, 1]),
+        np.mean(arr_red[:, 1]), np.std(arr_red[:, 1]),
+    )
 
 def main():
     num_classes = 5     # classes: 0,1,2,3,4
@@ -22,8 +32,13 @@ def main():
     pca.fit(arr)
     arr_red = pca.transform(arr)
 
-    plt.scatter(arr_red[:, 0], arr_red[:, 1], c=c_labels)
-    plt.axis('equal')
+    f, ax = plt.subplots(1,1,figsize=(6,6))
+    ax.scatter(arr_red[:, 0], arr_red[:, 1], c=c_labels)
+    ax.axis('equal')
+    ax.set_xlim([-2, 2])
+    ax.set_ylim([-2, 2])
+    # ax.set_aspect('equal', 'box')
+    ax.set_title(pca_stats(arr_red))
     plt.show()
 
 
